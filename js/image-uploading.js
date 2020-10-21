b7fb00d2255a737c8204c455a173cf35e3efec8e
tikhomirova-a <tikhomirova.aa95@gmail.com>
2020-10-21 14:38:07 +0300

    complete effect slider

--- js/image-uploading.js
+++ js/image-uploading.js
@@ -14,7 +14,7 @@
     document.addEventListener(`keydown`, onUploadedImageEscPress);
     window.main.uploadImageForm.addEventListener(`click`, window.scale.onScaleControlClick);
     window.main.uploadedImage.addEventListener(`change`, window.effect.onEffectInputChange);
-    window.main.effectPin.addEventListener(`mouseup`, window.effect.onEffectPinMouseUp);
+    window.main.effectPin.addEventListener(`mousedown`, window.effect.onEffectPinMouseDown);
     window.main.hashtagInput.addEventListener(`input`, window.validation.onHashtagInputInput);
     window.main.hashtagInput.addEventListener(`submit`, window.validation.onHashtagInputSubmit);
     window.main.commentInput.addEventListener(`input`, window.validation.onCommentInputInput);
@@ -26,6 +26,7 @@
     document.removeEventListener(`keydown`, onUploadedImageEscPress);
     window.main.uploadImageForm.removeEventListener(`click`, window.scale.onScaleControlClick);
     window.main.uploadedImage.removeEventListener(`change`, window.effect.onEffectInputChange);
+    window.main.effectPin.removeEventListener(`mousedown`, window.effect.onEffectPinMouseDown);
     window.main.hashtagInput.removeEventListener(`input`, window.validation.onHashtagInputInput);
     window.main.hashtagInput.removeEventListener(`submit`, window.validation.onHashtagInputSubmit);
     window.main.commentInput.removeEventListener(`input`, window.validation.onCommentInputInput);
